ed02a7a499ed8c6b596be8b90e6c6732dc72dbc0
JasperTimm <jasper.timm@kreuzwerker.de>
2014-11-11 18:28:51 +0100

    Add GPG key install to rvm install

--- manifests/system.pp
+++ manifests/system.pp
@@ -8,7 +8,8 @@ class rvm::system($version=undef) {
 
   exec { 'system-rvm':
     path    => '/usr/bin:/usr/sbin:/bin',
-    command => "bash -c '/usr/bin/curl -Ls https://raw.githubusercontent.com/wayneeseguin/rvm/master/binscripts/rvm-installer -o /tmp/rvm-installer && \
+    command => "gpg2 --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3 && \
+                bash -c '/usr/bin/curl -Ls https://raw.githubusercontent.com/wayneeseguin/rvm/master/binscripts/rvm-installer -o /tmp/rvm-installer && \
                 chmod +x /tmp/rvm-installer && \
                 rvm_bin_path=/usr/local/rvm/bin rvm_man_path=/usr/local/rvm/man /tmp/rvm-installer --version ${actual_version} && \
                 rm /tmp/rvm-installer'",
